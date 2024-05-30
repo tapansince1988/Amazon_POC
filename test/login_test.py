73c84c5ee0cfb6cb767a0a14e6e3cd0c988d4c07
tapan <tapansince1988.com>
2024-05-30 08:49:41 +0530

    added another test

--- test/login_test.py
+++ test/login_test.py
@@ -1,7 +1,17 @@
 import time
 from pages.home_pg import HomePage
 
+def test_The_home_page_has_the_correct_title(driver):
+    hp = HomePage(driver)
+    hp.goto_homepage()
+    page_title = hp.get_page_title()
+    # print(page_title)
+    expected_title = 'Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in'
+    assert page_title == expected_title
+    # time.sleep(3)
 def test_to_authenticate_login_with_correct_credentials(driver):
     hp = HomePage(driver)
     hp.goto_homepage()
-    time.sleep(3)
+
+    # time.sleep(3)
+
