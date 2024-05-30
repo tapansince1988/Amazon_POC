6d452036b43d8124d2584aa1a1688570d8e7fbea
tapan <tapansince1988.com>
2024-05-30 06:45:26 +0530

    added base page

---
+++ pages/base_pg.py
@@ -0,0 +1,11 @@
+import seleniumpagefactory
+
+class Base_Page():
+    def __init__(self, driver):
+        self.driver = driver
+
+    def get_page_title(self):
+        return self.driver.title
+
+    def get_current_url(self):
+        return self.driver.get_current_url
